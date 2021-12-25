e853f942a99f73c7e8db7eac81434740d2473264
Alexander <aleksandr__sokol@mail.ru>
2021-12-25 18:09:55 +0500

    feat: Add models to application

--- training_api/models.py
+++ training_api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AdvUser(AbstractUser):
+    """
+    Advanced user model for project
+    """
+    age = models.PositiveSmallIntegerField(null=True)
+    sex = models.CharField(max_length=1,
+                           choices=(('M', 'Male'), ('F', 'Female')),
+                           blank=False,
+                           default='M')
+    height = models.PositiveSmallIntegerField(null=True)
+    weight = models.PositiveSmallIntegerField(null=True)
+    level = models.CharField(max_length=1,
+                             choices=(('J', 'Junior'), ('M', 'Middle'), ('S', 'Senior')),
+                             blank=False,
+                             default='J')
+
+
+class Workout(models.Model):
+    name = models.CharField(max_length=120, null=False)
+    description = models.TextField(null=True)
+    unit = models.CharField(max_length=1,
+                            choices=(('I', 'Iteration'), ('T', 'Time')),
+                            blank=False,
+                            default='I')
+
+
+class SetWorkout(models.Model):
+    name = models.CharField(max_length=120, null=False)
+
+    user = models.ForeignKey(AdvUser,
+                             related_name='user',
+                             on_delete=models.CASCADE,
+                             null=False,
+                             blank=False)
+
+    work = models.ManyToManyField(Workout, through='Repeated', through_fields=('set', 'work'))
+
+
+class Repeated(models.Model):
+    work = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    set = models.ForeignKey(SetWorkout, on_delete=models.CASCADE)
+    repeat = models.IntegerField()
