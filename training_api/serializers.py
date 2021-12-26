926c34bd53a220b45feadd4820caa2388eea5034
Alexander <aleksandr__sokol@mail.ru>
2021-12-26 15:12:55 +0500

    feat: Add REST to CRUD operation in workout table

--- training_api/serializers.py
+++ training_api/serializers.py
@@ -2,6 +2,12 @@ from django.db.models import CharField
 from rest_framework import serializers
 from rest_framework.fields import Field
 
-# from .models import Person, Position
+from .models import AdvUser, Repeated, Workout, SetWorkout
 
 from django.core.exceptions import ObjectDoesNotExist
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = '__all__'
--- training_api/views.py
+++ training_api/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from .models import AdvUser, Repeated, Workout, SetWorkout
+from .serializers import WorkoutSerializer
 
-# Create your views here.
+
+class ListListObjects(ListCreateAPIView):
+    """
+    Переопределяет метод create класса ListCreateAPIView
+    для создания через Post запрос списка объектов
+    """
+
+    def create(self, request, *args, **kwargs):
+        many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, headers=headers)
+
+    def get_queryset(self):
+        params = self.request.query_params.dict()
+        return super().get_queryset().filter(**params).all()
+
+
+class WorkoutView(ListListObjects):
+    """
+    Место рождения человека: Страна и город
+    Может передаваться информация об одном месте, так и информация о нескольких местах в виде списка
+    """
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+
+class SingleWorkoutView(RetrieveUpdateDestroyAPIView):
+    """
+    Место рождения человека: Страна и город
+    """
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
