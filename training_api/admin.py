3bcbeac08509ddb32306fa09e75ae7904eda4f34
Alexander <aleksandr__sokol@mail.ru>
2021-12-26 14:39:18 +0500

    style: Add tables for admin

--- training_api/admin.py
+++ training_api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-# from .models import Person, Position
+from .models import AdvUser, Workout, SetWorkout, Repeated
 
 
-# admin.site.register(Person)
+admin.site.register(AdvUser)
+admin.site.register(Workout)
+admin.site.register(SetWorkout)
+admin.site.register(Repeated)
